d31ff663c478f4d268878cba4ec0730d361e0446
SebSa12000 <sebastien.salgues@gmail.com>
2025-02-17 14:28:13 +0000

    **

--- variables_if_else_while/0-positive_or_negative.c
+++ variables_if_else_while/0-positive_or_negative.c
@@ -1,10 +1,9 @@
 #include <stdlib.h>
 #include <time.h>
 #include <stdio.h>
-/* more headers goes there */
-
-/*
+/**
  * main - test de valeur aleatoire
+ * permet de tester des valeurs aleatoire positive ou negative
  *
  * Return: Always 0 (Success)
  */
@@ -14,11 +13,13 @@ int main(void)
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-	/* your code goes there */
+	/* test if positive */
 	if (n > 0)
 		printf("%d is positive\n", n);
+	/* test if null */
 	else if (n == 0)
 		printf("%d is zero\n", n);
+	/* test if negative */
 	else
 		printf("%d is negative\n", n);
 	return (0);
