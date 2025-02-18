0a65579336d3e82971eb9681f08fa971b7998900
SebSa12000 <sebastien.salgues@gmail.com>
2025-02-18 13:23:26 +0000

    exercice 4

---
+++ variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - Affiche les caracteres de l'alphabet minuscule sans q et e
+ *
+ * Return: Always (Success)
+ */
+int main(void)
+{
+	char caractere = 'a';
+
+	do {
+		if ((caractere != 'q') || (caractere != 'e'))
+			putchar(caractere);
+		caractere++;
+	} while (caractere <= 'z');
+	printf("\n");
+	return (0);
+}
