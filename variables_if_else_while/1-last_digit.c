9cbedf9f069df671cc5e5fae71313656b7d1caa5
SebSa12000 <sebastien.salgues@gmail.com>
2025-02-18 08:48:51 +0000

    commit last digit

---
+++ variables_if_else_while/1-last_digit.c
@@ -0,0 +1,25 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/** 
+ * test last digit d'un nombre aléatoire 
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	/* your code goes there */
+	printf("Last digit of %d is %d ", n, n%10);
+	if (n>5)
+		printf("and is greater than 5\n");
+	else if (n==0)
+		printf("and is 0\n");
+	else
+		printf("and is less than 6 and not 0\n");
+	return (0);
+}
