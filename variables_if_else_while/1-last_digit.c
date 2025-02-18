9446999bc50081109403b052dcda1a8e79aebf1d
SebSa12000 <sebastien.salgues@gmail.com>
2025-02-18 08:56:06 +0000

    commit last digit erreur modulo

--- variables_if_else_while/1-last_digit.c
+++ variables_if_else_while/1-last_digit.c
@@ -2,8 +2,8 @@
 #include <stdlib.h>
 #include <time.h>
 
-/** 
- * test last digit d'un nombre aléatoire 
+/**
+ * test last digit d'un nombre aléatoir
  *
  * Return: Always 0 (Success)
  */
@@ -14,10 +14,10 @@ int main(void)
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 	/* your code goes there */
-	printf("Last digit of %d is %d ", n, n%10);
-	if ((n%10)>5)
+	printf("Last digit of %d is %d ", n, n % 10);
+	if ((n % 10) > 5)
 		printf("and is greater than 5\n");
-	else if ((n%10)==0)
+	else if ((n % 10) == 0)
 		printf("and is 0\n");
 	else
 		printf("and is less than 6 and not 0\n");
