403d7152b0529fe0c6cdfe0da9970cbf2d06a6b7
SebSa12000 <sebastien.salgues@gmail.com>
2025-02-18 14:07:46 +0000

    affiche les chiffres de 0  9

---
+++ variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - Affiche les chiffres de 0 a 9 
+ *
+ * Return: Always (Success)
+ */
+int main(void)
+{
+	char caractere = '0';
+
+	do {
+		/* affichage du caracte¨re */
+		putchar(caractere);
+		/* increment */
+		caractere++;
+
+	} while (caractere <= '9');
+	printf("\n");
+	return (0);
+}
