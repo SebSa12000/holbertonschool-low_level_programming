1182285a9047d052cc9af5353f8fbcf40c74f108
SebSa12000 <sebastien.salgues@gmail.com>
2025-02-19 15:35:59 +0000

    alphabet

---
+++ functions_nested_loops/1-alphabet.c
@@ -0,0 +1,14 @@
+/**
+ * main - test d'affichage
+ * permet de tester des valeurs aleatoire positive ou negative
+ *
+ * Return: Always 0 (Success)
+ */
+void print_alphabet(void)
+{
+	char c;
+	for (c='a';c<='z';c++)
+		_putchar(c);
+	_putchar('\n');
+	return (0);
+}
