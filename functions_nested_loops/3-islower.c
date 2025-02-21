8072816a25d3c74fa44a8d66c955e284c7cba03b
SebSa12000 <sebastien.salgues@gmail.com>
2025-02-21 08:33:11 +0000

    mise en forme betty

--- functions_nested_loops/3-islower.c
+++ functions_nested_loops/3-islower.c
@@ -1,4 +1,11 @@
 #include "main.h"
+
+/**
+ * _islower - test si un caractere est minuscule ou pas
+ * @c: caractere a controler
+ *
+ * Return: Always 1/0 (Success)
+ */
 int _islower(int c)
 {
 	if (c >= 'a' && c <= 'z')
