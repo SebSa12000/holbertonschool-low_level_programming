9c7ac3b55c836cb3b751864df0ed7ab43d17d92f
SebSa12000 <sebastien.salgues@gmail.com>
2025-02-21 14:42:34 +0000

    fix erreur

--- functions_nested_loops/7-print_last_digit.c
+++ functions_nested_loops/7-print_last_digit.c
@@ -9,15 +9,16 @@
 int print_last_digit(int i)
 {
 	long valeur = i;
-	if ( valeur < 0 ) 
+
+	if (valeur < 0)
 	{
-		valeur = ( (long)(valeur*-1) )  % 10;
+		valeur = ((long)(valeur * -1)) % 10;
 	}
-	else 
+	else
 	{
 		valeur = valeur % 10;
 	}
-	if ( valeur< 0 || valeur> 9)
+	if (valeur < 0 || valeur > 9)
 	{
 		_putchar('E');
 	}
