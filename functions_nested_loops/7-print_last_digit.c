e3235d224549d6b94f5917fbc4a9bc41d48ec411
SebSa12000 <sebastien.salgues@gmail.com>
2025-02-21 14:20:27 +0000

    ajout de putchar

--- functions_nested_loops/7-print_last_digit.c
+++ functions_nested_loops/7-print_last_digit.c
@@ -8,5 +8,11 @@
  */
 int print_last_digit(int i)
 {
-	return i % 10;
+	int valeur = 0;
+	if ( i < 0 ) 
+		valeur = ((-1) * i ) % 10;
+	else 
+		valeur = i % 10;
+	_putchar(valeur + '0');
+	return (valeur);
 }
