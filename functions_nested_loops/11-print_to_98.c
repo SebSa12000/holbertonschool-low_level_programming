82a34970234fb2f6a160ef3f7f8c2e431fbb5c71
SebSa12000 <sebastien.salgues@gmail.com>
2025-02-22 10:30:15 +0000

    affiche jusqu'a 98 split

--- functions_nested_loops/11-print_to_98.c
+++ functions_nested_loops/11-print_to_98.c
@@ -1,6 +1,39 @@
 #include "main.h"
 
 /**
+ * print_to_98_sup98 - table de multiplication
+ * @n : valeur de depart
+ *
+ */
+void print_to_98_sup98(int n)
+{
+	int i = 0;
+
+	for (i = n; i >= 98; i--)
+	{
+		if (i >= 100)
+		{
+			_putchar((i / 100) + '0');
+			_putchar(((i % 100) / 10) + '0');
+			_putchar((i % 10) + '0');
+		}
+		else if (i >= 10)
+		{
+			_putchar((i / 10) + '0');
+			_putchar((i % 10) + '0');
+		}
+		else if (i < 10)
+		{
+			_putchar((i % 10) + '0');
+		}
+		if (i != 98)
+		{
+			_putchar(',');
+			_putchar(' ');
+		}
+	}
+}
+/**
  * print_to_98 - table de multiplication
  * @n : valeur de depart
  *
@@ -10,29 +43,7 @@ void print_to_98(int n)
 	int i = 0;
 
 	if (n >= 98)
-		for (i = n; i >= 98; i--)
-		{
-			if (i >= 100)
-			{
-				_putchar((i / 100) + '0');
-				_putchar(((i % 100) / 10) + '0');
-				_putchar((i % 10) + '0');
-			}
-			else if (i >= 10)
-			{
-				_putchar((i / 10) + '0');
-				_putchar((i % 10) + '0');
-			}
-			else if (i < 10)
-			{
-				_putchar((i % 10) + '0');
-			}
-			if (i != 98)
-			{
-				_putchar(',');
-				_putchar(' ');
-			}
-		}
+		print_to_98_sup98(n);
 	else if (n < 98)
 		for (i = n; i <= 98; i++)
 		{
