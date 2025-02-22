a61c8d2ddeab262638cbcd99af25432ae1252944
SebSa12000 <sebastien.salgues@gmail.com>
2025-02-22 09:14:32 +0000

    somme d'entier

---
+++ functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * add - table de multiplication
+ * @a: entier a controler
+ * @b: entier a controler
+ *
+ * Return: somme de a et b
+ */
+int add(int a, int b)
+{
+	return (a + b);
+}
