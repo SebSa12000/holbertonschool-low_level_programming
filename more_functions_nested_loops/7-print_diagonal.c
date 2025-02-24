f06918da61c644b39709d304ef874b0089c3aacb
SebSa12000 <sebastien.salgues@gmail.com>
2025-02-24 14:27:00 +0000

    ligne en diagonale

---
+++ more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,29 @@
+#include "main.h"
+/**
+ * print_diagonal - affiche une ligne en diagonale 
+ *@n : nombre de \
+ *
+ */
+void print_diagonal(int n)
+{
+	int i = 0;
+	int last = 0;
+
+	if (n > 0)
+	{
+		do 
+		{
+			last++;
+			for (i = 0; i < last; i++)
+			{
+				if ( i < last - 1 )
+					_putchar(' ');
+				else
+					_putchar('\\');
+			}
+			_putchar('\n');
+		} while (last < n);
+	}
+	else
+		_putchar('\n');
+}
