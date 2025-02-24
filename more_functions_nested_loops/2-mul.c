d53b718e3efcfa6b6d56e6000c5dee1e0462cd65
SebSa12000 <sebastien.salgues@gmail.com>
2025-02-24 10:07:41 +0000

    multiplie

---
+++ more_functions_nested_loops/2-mul.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * mul - multiplication
+ * @a : a valeur a multiplier
+ * @b : b valeur a multiplier
+ *
+ * Return: resultat
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
