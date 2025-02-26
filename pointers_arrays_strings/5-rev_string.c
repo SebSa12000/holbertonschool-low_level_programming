8bc3544d436dfbed6804fec4c6b11428b6011242
SebSa12000 <sebastien.salgues@gmail.com>
2025-02-26 13:53:12 +0000

    inverse une string

--- pointers_arrays_strings/5-rev_string.c
+++ pointers_arrays_strings/5-rev_string.c
@@ -1,6 +1,7 @@
 #include "main.h"
 #include <stdlib.h>
 #include <string.h>
+#include <stdio.h>
 /**
  * rev_string - inverse une chaine
  * @s: chaine de caractere
@@ -10,26 +11,27 @@ void rev_string(char *s)
 {
 	char *inc = s;
 	int maxlen = 0, cpt = 0, len = 0, j = 0;
-	char tempo[10] = "My school"; 
-	char *ptempo = tempo;
-
+	char *tempo = NULL;
+	char *ptempo = NULL;
 	while (*inc != '\0')
 	{
 		inc++;
 		len++;
 	}
 	maxlen = len;
-	while (len > 0)
+	tempo = malloc(sizeof(char) * (maxlen + 1));
+	if (tempo != NULL)
 	{
-		
-		len--;
-		inc--;
-		ptempo[cpt] = *inc;
-		cpt++;
+		ptempo = tempo;
+		while (len > 0)
+		{
+			len--;
+			inc--;
+			ptempo[cpt] = *inc;
+			cpt++;
+		}
+		for (j = 0; j < maxlen; j++)
+			s[j] = ptempo[j];
+		free(tempo);
 	}
-
-	for (j = 0; j < maxlen; j++)
-		s[j] = ptempo[j];
-
-
 }
