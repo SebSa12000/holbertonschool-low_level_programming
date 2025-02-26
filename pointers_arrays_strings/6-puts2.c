1f4d381791a208237b8dba59580382a366f15758
SebSa12000 <sebastien.salgues@gmail.com>
2025-02-26 15:14:36 +0000

    puts 2

---
+++ pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ * _puts2 - longueur d'une chaine
+ * @s: chaine de caractere
+ *
+ */
+void _puts2(char *s)
+{
+	char *inc = s;
+	int flag = 1;
+
+	while (*inc != '\0')
+	{
+		if (flag == 1)
+		{
+			_putchar(*inc);
+			flag = 0;
+		}
+		else
+		{
+			flag = 1;
+		}
+		inc++;
+	}
+	_putchar('\n');
+}
