73285478152816dd8c2713ba4fcb6a3b1e46866d
SebSa12000 <sebastien.salgues@gmail.com>
2025-02-28 13:55:51 +0000

    atoi

--- pointers_arrays_strings/100-atoi.c
+++ pointers_arrays_strings/100-atoi.c
@@ -10,10 +10,15 @@ int _atoi(char *s)
 	int retour = 0;
 	int i = 0;
 	int flagneg = 0;
+	int fini = 0;
 
-	while (s[i] != '\0')
+	while ((s[i] != '\0') && (fini == 0))
 	{
-		if ((s[i] >= '0') && (s[i] <= '9'))
+		if ((s[i] == ' ') && (fini == 0) && (retour > 0))
+		{
+			fini = 1;
+		}
+		else if ((s[i] >= '0') && (s[i] <= '9'))
 		{
 			retour = retour * 10 + ((char)s[i] - 48);
 		}
