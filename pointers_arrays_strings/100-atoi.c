9586b67efd59f479512c9d5b8c23556f27ecd691
SebSa12000 <sebastien.salgues@gmail.com>
2025-02-28 18:53:26 +0000

    betty

--- pointers_arrays_strings/100-atoi.c
+++ pointers_arrays_strings/100-atoi.c
@@ -11,19 +11,18 @@ int _atoi(char *s)
 	int retour = 0;
 	int i = 0;
 	int flagneg = 0;
-	int traitement = 1; 
+	int traitement = 1;
 
 	while ((s[i] != '\0') && (traitement == 1))
 	{
 		if ((s[i] > 47) && (s[i] < 58))
 		{
 			if (s[i] != '-')
-			{	
-				if ( retour < 0 )
+			{
+				if (retour < 0)
 					retour = (retour * 10) - (s[i] - '0');
 				else
 					retour = (retour * 10) + (s[i] - '0');
-					
 			}
 			if (flagneg == 1)
 			{
@@ -40,9 +39,7 @@ int _atoi(char *s)
 		}
 		if (i > 0 && traitement == 1 && retour > 0 && s[i] == ' ')
 			traitement = 0;
-		
 		i++;
-		
 	}
 	return (retour);
 }
