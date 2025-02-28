5f86363d00c109537e28ca845d71a8624d2dd2b4
SebSa12000 <sebastien.salgues@gmail.com>
2025-02-28 08:10:26 +0000

    affiche un array

--- pointers_arrays_strings/8-print_array.c
+++ pointers_arrays_strings/8-print_array.c
@@ -10,12 +10,13 @@ void print_array(int *a, int n)
 {
 	int i = 0;
 
-	for (i = 0; i < n; i++)
-	{
-		printf("%d", a[i]);
-		if (i < (n - 1))
-			printf(", ");
-		else
-			printf("\n");
-	}
+	if (n > 0)
+		for (i = 0; i < n; i++)
+		{
+			printf("%d", a[i]);
+			if (i < (n - 1))
+				printf(", ");
+			else
+				printf("\n");
+		}
 }
