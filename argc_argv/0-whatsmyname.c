f376739f6f1a2edb50784bfd608d73e18ba4729c
SebSa12000 <sebastien.salgues@gmail.com>
2025-03-07 11:09:23 +0000

    betty

---
+++ argc_argv/0-whatsmyname.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - check the code
+ * @ac: nombre d'argument
+ * @av: tableau de string
+ *
+ * Return: Always 0.
+ */
+int main(int ac, char **av)
+{
+	(void)ac;
+	printf("%s\n", av[0]);
+	return (0);
+}
