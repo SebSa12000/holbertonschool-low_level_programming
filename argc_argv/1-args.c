453e8bebfb369c044e361f45b7c09ded6066cdcf
SebSa12000 <sebastien.salgues@gmail.com>
2025-03-07 12:42:28 +0000

    Arguments

---
+++ argc_argv/1-args.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - check the code
+ * @ac: nombre d'argument
+ * @av: tableau de string
+ *
+ * Return: Always 0.
+ */
+int main(int ac, char **av)
+{
+	(void)av;
+	printf("%d\n", ac);
+	return (0);
+}
