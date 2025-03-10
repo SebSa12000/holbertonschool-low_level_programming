a751a989283728cecc785e821dda76cadd4075b4
SebSa12000 <sebastien.salgues@gmail.com>
2025-03-10 10:15:52 +0000

    test crash

--- malloc_free/0-create_array.c
+++ malloc_free/0-create_array.c
@@ -12,12 +12,16 @@ char *create_array(unsigned int size, char c)
 	unsigned int i = 0;
 	char *retour = (char *)malloc(sizeof(char) * size);
 
-	if (size > 0)
+	if (retour != NULL)
 	{
-		for (i = 0; i < size; i++)
-			retour[i] = c;
-		return (retour);
+		if (size > 0)
+		{
+			for (i = 0; i < size; i++)
+				retour[i] = c;
+			return (char *)(retour);
+		}
+		else
+			return (char *)(NULL);
 	}
-	else
-		return (char *)(NULL);
+	return (char *)(NULL);
 }
