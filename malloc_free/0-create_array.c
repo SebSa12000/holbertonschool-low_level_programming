4cf3747d701a57799027073086ea797a01690c6a
SebSa12000 <sebastien.salgues@gmail.com>
2025-03-10 10:03:30 +0000

    cree un tableau

---
+++ malloc_free/0-create_array.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * create_array - cree un tableau
+ * @size : longueur
+ * @c : caractere
+ *
+ * Return : pointeur sur tableau
+ */
+char *create_array(unsigned int size, char c)
+{
+	int i = 0;
+	char *retour = (char *)malloc(sizeof(char) * size);
+
+	if (size > 0)
+	{
+		for (i = 0; i < size; i++)
+			retour[i] = c;
+		return (retour);
+	}
+	else
+		return (NULL);
+}
