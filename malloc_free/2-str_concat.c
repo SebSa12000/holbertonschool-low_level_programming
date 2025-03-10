a394e9e51113bf05ac4fbc71df8ceaf5141bb66b
SebSa12000 <sebastien.salgues@gmail.com>
2025-03-10 13:57:46 +0000

    concat de chaine

---
+++ malloc_free/2-str_concat.c
@@ -0,0 +1,49 @@
+#include "main.h"
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+
+/**
+ * str_concat - concat de chaine
+ * @str1 : chaine1
+ * @str2 : chaine2
+ *
+ * Return: pointeur sur chaine
+ */
+char *str_concat(char *str1, char *str2)
+{
+	unsigned int i = 0;
+	unsigned int longueur = 1;
+	char *pointeurstr1 = str1;
+	char *pointeurstr2 = str2;
+	char *pointeurretour = NULL;
+
+	longueur = strlen(str1) + strlen(str2) + 1;
+	/* si la longueur est positive */
+	if (longueur > 0)
+	{
+		char *retour = (char *)malloc(sizeof(char) * longueur);
+
+	/* si la longueur est positive */
+		pointeurretour = retour;
+		if (retour != NULL)
+		{
+			i = 0;
+			while (pointeurstr1 != NULL && *pointeurstr1 != '\0')
+			{
+				pointeurretour[i] = *pointeurstr1;
+				i++;
+				pointeurstr1++;
+			}
+			while (pointeurstr2 != NULL && *pointeurstr2 != '\0')
+			{
+				pointeurretour[i] = *pointeurstr2;
+				i++;
+				pointeurstr2++;
+			}
+			pointeurretour[i] = '\0';
+			return (retour);
+		}
+	}
+	return ((char *)(NULL));
+}
