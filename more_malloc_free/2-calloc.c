598e00288546a22f1f511c44c27f13bb82651312
SebSa12000 <sebastien.salgues@gmail.com>
2025-03-12 12:59:04 +0000

    fix calloc

---
+++ more_malloc_free/2-calloc.c
@@ -0,0 +1,30 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+ * _calloc - cree un tableau
+ * @nmemb : longueur
+ * @size : size 
+ *
+ * Return: pointeur sur tableau
+ */
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	unsigned int i = 0;
+
+	void *retour;
+
+	if (size <= 0 || nmemb <= 0)
+		return (NULL);
+	retour = malloc(size * nmemb);
+	int *pretour = retour;
+	if (retour != NULL)
+	{
+		for (i = 0; i < size; i++)
+		{
+			pretour = 0;
+			pretour++;
+		}
+		return (retour);
+	}
+	return (NULL);
+}
