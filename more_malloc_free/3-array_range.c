ff8dd34b111a9943917ad210763c1f2612f21e1a
SebSa12000 <sebastien.salgues@gmail.com>
2025-03-12 15:15:21 +0000

    array range

---
+++ more_malloc_free/3-array_range.c
@@ -0,0 +1,33 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+ * array_range - cree un tableau entre min et max
+ * @min : longueur
+ * @max : size
+ *
+ * Return: pointeur sur tableau
+ */
+int *array_range(int min, int max)
+{
+	int i = 0;
+	int *retour;
+	int *pretour;
+	int length = 0;
+
+	if (min < 0 || max < 0 || min > max)
+		return (NULL);
+
+	length = max - min + 1;
+	retour = malloc(sizeof(int) * (length));
+	if (retour != NULL)
+	{
+		pretour = retour;
+		for (i = 0; i < (length); i++)
+		{
+			*pretour = i;
+			pretour++;
+		}
+		return (retour);
+	}
+	return (NULL);
+}
