577207161b2969555dc3089371bf18c7795f0455
SebSa12000 <sebastien.salgues@gmail.com>
2025-03-14 09:59:39 +0000

    init dog

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -1,6 +1,6 @@
+#include <stddef.h>
 #ifndef _DOG_H_
 #define _DOG_H_
-
 /**
  * struct dog - DÃcrit un chie
  * @name: nom
@@ -9,10 +9,11 @@
  *
  * Description: Longer description
  */
-struct dog
+typedef struct dog
 {
 	char *name;
 	float age;
 	char *owner;
-};
+} dog_t;
+void init_dog(struct dog *d, char *name, float age, char *owner);
 #endif
