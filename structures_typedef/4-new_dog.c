6138a4e7ee90f4c78f9112da3bf4f96ac4affeef
SebSa12000 <sebastien.salgues@gmail.com>
2025-03-14 14:09:46 +0000

    new dog

--- structures_typedef/4-new_dog.c
+++ structures_typedef/4-new_dog.c
@@ -9,18 +9,40 @@
 dog_t *new_dog(char *name, float age, char *owner)
 {
 	dog_t *ptr = malloc(sizeof(dog_t));
+	char *ptrname = name, *ptrowner = owner;
+	char *ptrname2;
 
 	if (ptr != NULL)
 	{
 		if (name != NULL)
 		{
-			ptr->name = strcpy(name);
+			ptr->name = malloc(sizeof(name) + 1);
+			if (ptr->name != NULL)
+			{
+				ptrname2 = name;
+				while (*ptrname2 != '\0')
+				{
+					*ptrname = *ptrname2;
+					ptrname++;
+					ptrname2++;
+				}
+			}
 		}
-
 		ptr->age = age;
 		if (owner != NULL)
 		{
-			ptr->owner = strcpy(owner);
+			ptr->owner = malloc(sizeof(owner) + 1);
+			if (ptr->owner != NULL)
+			{
+				ptrname = ptr->owner;
+				ptrname2 = owner;
+				while (*ptrname2 != '\0')
+				{
+					*ptrname = *ptrname2;
+					ptrname++;
+					ptrname2++;
+				}
+			}
 		}
 		return (ptr);
 	}
