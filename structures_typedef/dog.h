aa6d4cd59f74ac5495fecc873ad75cc922d2c95a
SebSa12000 <sebastien.salgues@gmail.com>
2025-03-14 08:41:19 +0000

    initialisation

---
+++ structures_typedef/dog.h
@@ -0,0 +1,14 @@
+/**
+ * struct dog - DÃcrit un chien 
+ * @name: nom 
+ * @age: age 
+ * @owner: owner 
+ *
+ * Description: Longer description
+ */
+struct dog 
+{
+	char *name;	
+	int age;
+	char *owner;
+};
