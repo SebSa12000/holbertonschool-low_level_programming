6b8a6270e6c32184e0fb1434e0584fb90aebe056
SebSa12000 <sebastien.salgues@gmail.com>
2025-03-14 09:54:04 +0000

    init dog

---
+++ structures_typedef/1-init_dog.c
@@ -0,0 +1,17 @@
+#include "dog.h"
+/**
+ * init_dog - Initialisation
+ * @d: structure dog
+ * @name: nom
+ * @age: age
+ * @owner: proprietaire
+ */
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	if (d != NULL)
+	{
+		(*d).name = name;
+		(*d).age = age;
+		(*d).owner = owner;
+	}
+}
