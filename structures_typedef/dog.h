1989a9f224321836e27c6ffaa153362eced03215
SebSa12000 <sebastien.salgues@gmail.com>
2025-03-14 10:21:18 +0000

    print dog

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -1,6 +1,7 @@
 #ifndef _DOG_H_
 #define _DOG_H_
 #include <stddef.h>
+#include <stdio.h>
 /**
  * struct dog - DÃcrit un chie
  * @name: nom
@@ -16,4 +17,5 @@ typedef struct dog
 	char *owner;
 } dog_t;
 void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
 #endif
