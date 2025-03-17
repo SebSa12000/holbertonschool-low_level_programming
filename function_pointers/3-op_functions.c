adc6339b6ed9fcddf3d0ad78eabe17afbcce3c04
SebSa12000 <sebastien.salgues@gmail.com>
2025-03-17 13:56:03 +0000

    Operations

---
+++ function_pointers/3-op_functions.c
@@ -0,0 +1,59 @@
+/**
+ * op_add - add operation
+ * @a: first integer
+ * @b: second integer
+ *
+ * Return: addition
+ */
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * op_sub - sub operation
+ * @a: first integer
+ * @b: second integer
+ *
+ * Return: addition
+ */
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ * op_mul - multiply operation
+ * @a: first integer
+ * @b: second integer
+ *
+ * Return: addition
+ */
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+ * op_div - divide operation
+ * @a: first integer
+ * @b: second integer
+ *
+ * Return: addition
+ */
+int op_div(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * op_mod - mod operation
+ * @a: first integer
+ * @b: second integer
+ *
+ * Return: addition
+ */
+int op_mod(int a, int b)
+{
+	return (a % b);
+}
