38d9bfaaa9882c1e50dfd22855e2d57818f34f2b
SebSa12000 <sebastien.salgues@gmail.com>
2025-03-17 13:26:27 +0000

    checker

--- function_pointers/2-int_index.c
+++ function_pointers/2-int_index.c
@@ -12,7 +12,7 @@ int int_index(int *array, size_t size, int (*cmp)(int))
 	size_t i = 0;
 	int retour = 0;
 
-	if (size <= 0)
+	if ((size <= 0) || (array == NULL))
 		return (-1);
 	for (i = 0; i < size; i++)
 	{
