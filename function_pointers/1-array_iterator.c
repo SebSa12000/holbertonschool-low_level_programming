667f5fad47e305b541cc51c79fff307923055ea0
SebSa12000 <sebastien.salgues@gmail.com>
2025-03-17 11:06:50 +0000

    display array

---
+++ function_pointers/1-array_iterator.c
@@ -0,0 +1,17 @@
+#include <stddef.h>
+/**
+ * array_iterator - Array iterator
+ * @array: array to display
+ * @size: size of array
+ * @action: function to do
+ *
+ */
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	size_t i = 0;
+
+	for (i = 0; i < size; i++)
+	{
+		action(array[i]);	
+	}	
+}
