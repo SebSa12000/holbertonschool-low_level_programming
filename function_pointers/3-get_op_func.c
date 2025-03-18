7a382b72cb5030324486321737aedbd25d2716ba
SebSa12000 <sebastien.salgues@gmail.com>
2025-03-18 10:16:43 +0000

    operator on function

--- function_pointers/3-get_op_func.c
+++ function_pointers/3-get_op_func.c
@@ -1,6 +1,12 @@
 #include "3-calc.h"
 #include <stddef.h>
 #include <string.h>
+/**
+ * get_op_func - return un operator on function
+ * @s: name of operator
+ *
+ * Return: Function
+ */
 int (*get_op_func(char *s))(int, int)
 {
 	int i = 0;
@@ -15,9 +21,9 @@ int (*get_op_func(char *s))(int, int)
 
 	while (ops[i].op != NULL)
 	{
-		if (strcmp(ops[i].op, s)==0)
-			return ops[i].f;
+		if (strcmp(ops[i].op, s) == 0)
+			return (ops[i].f);
 		i++;
-	}	
-	return 0;
+	}
+	return (0);
 }
