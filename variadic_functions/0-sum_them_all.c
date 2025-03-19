b1bdf5657e1a80f84d659401f70857d8bce86297
SebSa12000 <sebastien.salgues@gmail.com>
2025-03-19 08:22:14 +0000

    function with variables parameters

---
+++ variadic_functions/0-sum_them_all.c
@@ -0,0 +1,23 @@
+#include <stdarg.h>
+/** 
+ * sum_them_all
+ * @nb : number of parameter
+ *
+ * Return: Somme totale
+ */
+int sum_them_all(const unsigned int nb, ...)
+{
+	int retour = 0, c = 0;
+	unsigned int i = 0;
+	va_list ap ;
+	if (nb == 0)
+		return (0);
+
+	va_start(ap, nb);
+	for (i = 0; i < nb; i++)
+	{
+		c = va_arg (ap, int);
+		retour += c;
+	}
+	return (retour);
+}
---
+++ variadic_functions/variadic_functions.h
@@ -0,0 +1,4 @@
+#ifndef _functions_
+#define _functions_
+int sum_them_all(const unsigned int n, ...);
+#endif
