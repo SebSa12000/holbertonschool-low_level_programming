11d43112088bb092991a35324298fc46e63aff22
SebSa12000 <sebastien.salgues@gmail.com>
2025-03-19 08:26:05 +0000

    fix betty

--- variadic_functions/0-sum_them_all.c
+++ variadic_functions/0-sum_them_all.c
@@ -1,6 +1,6 @@
 #include <stdarg.h>
-/** 
- * sum_them_all
+/**
+ * sum_them_all - sum of variadic function
  * @nb : number of parameter
  *
  * Return: Somme totale
@@ -9,14 +9,15 @@ int sum_them_all(const unsigned int nb, ...)
 {
 	int retour = 0, c = 0;
 	unsigned int i = 0;
-	va_list ap ;
+	va_list ap;
+
 	if (nb == 0)
 		return (0);
 
 	va_start(ap, nb);
 	for (i = 0; i < nb; i++)
 	{
-		c = va_arg (ap, int);
+		c = va_arg(ap, int);
 		retour += c;
 	}
 	return (retour);
