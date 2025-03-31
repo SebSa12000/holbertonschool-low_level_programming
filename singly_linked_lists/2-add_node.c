250ff140a7ca3da9b5744467bcb10d7c098109f7
SebSa12000 <sebastien.salgues@gmail.com>
2025-03-31 13:25:24 +0000

    add node

---
+++ singly_linked_lists/2-add_node.c
@@ -0,0 +1,33 @@
+#include "lists.h"
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+
+/**
+ * add_node - add a new node
+ * @head : head
+ * @str : string
+ *
+ * Return: size
+ */
+list_t *add_node(list_t **head, const char *str)
+{
+	list_t *new_node;
+	char *string;
+	string = strdup(str);
+	if (string == NULL)
+		return (NULL);
+
+	new_node = malloc(sizeof(list_t));
+	if (new_node == NULL)
+		return (NULL);
+	if (head != NULL)
+		new_node->next = *head;
+	else
+		new_node->next = NULL;
+	new_node->str = string;
+	new_node->len = _strlen(string);
+	*head = new_node;
+	return (new_node);
+}
