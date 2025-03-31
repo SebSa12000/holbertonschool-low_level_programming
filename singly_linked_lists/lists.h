900c3222dbfedd887ac9de3b1100e9e05bce4138
SebSa12000 <sebastien.salgues@gmail.com>
2025-03-31 08:54:25 +0000

    betty

--- singly_linked_lists/lists.h
+++ singly_linked_lists/lists.h
@@ -1,4 +1,5 @@
-
+#ifndef _lists_
+#define _lists_
 #include <stddef.h>
 /**
  * struct list_s - singly linked list
@@ -19,3 +20,4 @@ list_t *add_node_end(list_t **head, const char *str);
 list_t *add_node(list_t **head, const char *str);
 size_t list_len(const list_t *h);
 size_t print_list(const list_t *h);
+#endif
