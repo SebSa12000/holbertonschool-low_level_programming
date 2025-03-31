c36132a1b16444aaa1b52307b0311399c5b4b19c
SebSa12000 <sebastien.salgues@gmail.com>
2025-03-31 10:04:35 +0000

    simplify code

--- singly_linked_lists/0-print_list.c
+++ singly_linked_lists/0-print_list.c
@@ -1,68 +1,6 @@
 #include "lists.h"
-
-void reverse(char s[]);
-
-/**
- * itoa - convertir n en caractères dans s
- * @n: value to convert
- * @s: string buffer
- *
- */
-void itoa(long n, char s[])
-{
-	long i, sign;
-
-	sign = n;
-	if (sign < 0)
-		/* enregistrer le signe */
-		n = -n;
-	/* rendre n positif */
-	i = 0;
-	do {
-		s[i++] = n % 10 + '0';
-		/* obtenir le chiffre suivant */
-	} while ((n /= 10) > 0); /* le supprimer */
-	if (sign < 0)
-		s[i++] = '-';
-	s[i] = '\0';
-	reverse(s);
-}
-
-/**
- * _strlen - calculate the length of a string
- * @s: string to calculate
- *
- * Return: length
- */
-int _strlen(char *s)
-{
-	int retour = 0;
-
-	while (*s != '\0')
-	{
-		s++;
-		retour++;
-	}
-	return (retour);
-}
-
-/**
- * reverse - reverse a string
- * @s: string to reverse
- */
-void reverse(char s[])
-{
-	int i, j;
-	char c;
-
-	for (i = 0, j = _strlen(s) - 1; i < j; i++, j--)
-	{
-		c = s[i];
-		s[i] = s[j];
-		s[j] = c;
-	}
-}
-
+#include <stdio.h>
+#include <string.h>
 
 /**
  * print_list - print a list chain
@@ -72,9 +10,7 @@ void reverse(char s[])
  */
 size_t print_list(const list_t *h)
 {
-	char buffer[100];
 	size_t length = 0;
-	char *string, *string2;
 
 	if (h != NULL)
 	{
@@ -82,31 +18,12 @@ size_t print_list(const list_t *h)
 		{
 			if (h->str == NULL)
 			{
-				_putchar('[');
-				_putchar('0');
-				_putchar(']');
-				string = "(nil)";
+				printf("[0] (nil)\n");
 			}
 			else
 			{
-				string = h->str;
-				_putchar('[');
-				itoa(_strlen(string), buffer);
-				string2 = buffer;
-				while (*string2 != '\0')
-				{
-					_putchar(*string2);
-					string2++;
-				}
-				_putchar(']');
+				printf("[%d] %s\n", (int)strlen(h->str), h->str);
 			}
-			_putchar(' ');
-			while (*string != '\0')
-			{
-				_putchar(*string);
-				string++;
-			}
-			_putchar('\n');
 			length++;
 			h = h->next;
 		}
