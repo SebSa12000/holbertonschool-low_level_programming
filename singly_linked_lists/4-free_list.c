5db3b65d57a110c791688c0dda2612e68a730374
SebSa12000 <sebastien.salgues@gmail.com>
2025-04-01 14:03:23 +0200

    free list

--- singly_linked_lists/4-free_list.c
+++ singly_linked_lists/4-free_list.c
@@ -12,14 +12,14 @@
 void free_list(list_t *head)
 {
 	/**/
-	list_t *new_node, *ptr;
+	list_t *ptr, *ptr2;
 
-	ptr = *head;
-	ptr2 = *head;
+	ptr = head;
+	ptr2 = head;
 	while (ptr != NULL && ptr->next != NULL)
 	{
 		ptr2 = ptr->next;
-		free(ptr->string);
+		free(ptr->str);
 		free(ptr);
 		ptr = ptr2;
 	}
