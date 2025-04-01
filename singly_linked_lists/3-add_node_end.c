4b328bf2b0112ee16338622ab75a410c5195b41d
SebSa12000 <sebastien.salgues@gmail.com>
2025-04-01 10:17:04 +0200

    Betty

--- singly_linked_lists/3-add_node_end.c
+++ singly_linked_lists/3-add_node_end.c
@@ -15,26 +15,21 @@ list_t *add_node_end(list_t **head, const char *str)
 {
 	list_t *new_node, *ptr;
 	char *string;
-	
+
 	string = strdup(str);
 	if (string == NULL)
-		return (NULL);
-	
+		return (NULL);	
 	new_node = malloc(sizeof(list_t));
 	if (new_node == NULL)
 		return (NULL);
 	ptr = *head;
-	
 	new_node->next = NULL;
-	
 	while (ptr != NULL && ptr->next != NULL)
 	{
 		ptr = ptr->next;
 	}
-	
 	if (ptr != NULL)
 		ptr->next = new_node;
-	
 	new_node->str = string;
 	new_node->len = _strlen(string);
 
