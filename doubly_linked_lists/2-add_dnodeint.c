235c28e478cee663c8098079f6325f3e1df63f8b
SebSa12000 <sebastien.salgues@gmail.com>
2025-04-02 09:47:51 +0200

    fix bug two elements

--- doubly_linked_lists/2-add_dnodeint.c
+++ doubly_linked_lists/2-add_dnodeint.c
@@ -18,7 +18,7 @@ dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 	new_node = malloc(sizeof(dlistint_t));
 	if (new_node == NULL)
 		return (NULL);
-	if (head != NULL)
+	if (*head != NULL)
 		new_node->next = *head;
 	else
 		new_node->next = NULL;
