cbebfab4b9e4e5a41143ec221da2068337345923
SebSa12000 <sebastien.salgues@gmail.com>
2025-04-03 14:40:59 +0200

    insert node in idx position

--- doubly_linked_lists/7-insert_dnodeint.c
+++ doubly_linked_lists/7-insert_dnodeint.c
@@ -83,6 +83,13 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 
 	new_node->n = n;
 	
+	if (idx == 0)
+	{
+		new_node->next = *h;
+		*h = new_node;
+		return new_node;
+	}
+	
 	ptr_node = get_dnodeint_at_index(*h, idx);
 	if (ptr_node == NULL)
 	{
