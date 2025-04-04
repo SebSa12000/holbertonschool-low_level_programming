61b53092771326836f23c733a0b41a14d20205d5
SebSa12000 <sebastien.salgues@gmail.com>
2025-04-04 18:41:53 +0200

    fix bug

--- file_io/3-cp.c
+++ file_io/3-cp.c
@@ -23,26 +23,34 @@ int main(int ac, char **av)
 		dprintf(2, "Usage: cp file_from file_to\n");
 		exit(97);
 	}
-
-	fdw = open(av[2], O_WRONLY | O_CREAT | O_TRUNC, 0600);
-
+	fdw = open(av[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
+	if (fdw < 0)
+	{
+		dprintf(2, " Error: Can't write to NAME_OF_THE_FILE%s\n", av[2]);
+		exit(99);
+	}
 	longueur = 10000 * sizeof(char);
 	buffer = malloc(longueur);
 	if (buffer == NULL)
 		exit(99);
-
-
 	fd = open(av[1], O_RDONLY);
-	length = read(fd, buffer, longueur);
-
-	write(fdw, buffer, length);
-	/* test if open fail */
 	if (fd < 0)
 	{
 		dprintf(2, "Error: Can't read from file %s\n", av[1]);
 		exit(98);
 	}
-	close(fd);
+	length = read(fd, buffer, longueur);
+	write(fdw, buffer, length);
+	if (close(fd) == -1)
+	{
+		dprintf(2, "Error: Can't close %d\n", fd);
+		exit(100);
+	}
+	if (close(fdw) == -1)
+	{
+		dprintf(2, "Error: Can't close %d\n", fdw);
+		exit(100);
+	}
 	close(fdw);
 	return (0);
 }
