ac1cbf20a6b2b4dbdaef7916c69b2d699ab860ed
SebSa12000 <sebastien.salgues@gmail.com>
2025-04-04 14:53:53 +0200

    remove some caracters

--- file_io/1-create_file.c
+++ file_io/1-create_file.c
@@ -2,6 +2,7 @@
 #include <fcntl.h>
 #include <unistd.h>
 #include <stdio.h>
+#include <string.h>
 /**
  * create_file - create a text file
  * @filename : name of file
@@ -12,11 +13,12 @@
 int create_file(const char *filename, char *text_content)
 {
 	int fd;
+	size_t length = 0;
 
 	if (filename == NULL)
 		return (-1);
 
-	fd = open(filename, O_WRONLY | O_CREAT, 0600 );
+	fd = open(filename, O_WRONLY | O_CREAT, 0600);
 
 	if (text_content == NULL)
 		text_content = "";
@@ -26,8 +28,8 @@ int create_file(const char *filename, char *text_content)
 	{
 		return (-1);
 	}
-	
-	write(fd, text_content, sizeof(text_content)-1);
+	length = (size_t)strlen(text_content);
+	write(fd, text_content, length);
 	close(fd);
 	return (1);
 }
