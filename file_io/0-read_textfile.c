ea5fea295783b77ab0e9432406e1f00bd104c2fa
SebSa12000 <sebastien.salgues@gmail.com>
2025-04-04 13:58:53 +0200

    betty + test length

--- file_io/0-read_textfile.c
+++ file_io/0-read_textfile.c
@@ -6,6 +6,8 @@
 #include <stdlib.h>
 /**
  * read_textfile - read a text file
+ * @filename : name of file
+ * @letters : nb letters
  *
  * Return: size read
  */
@@ -17,26 +19,29 @@ size_t read_textfile(const char *filename, size_t letters)
 	char *buffer;
 
 	if (filename == NULL)
-		return 0;
+		return (0);
 	fd = open(filename, O_RDONLY);
 
 	/* test if open fail */
 	if (fd < 0)
 	{
-		return 0;
+		return (0);
 	}
 	longueur = (int)letters;
 
-	buffer = malloc(sizeof(char)*longueur);
+	buffer = malloc(sizeof(char) * longueur);
 	if (buffer == NULL)
-		return -1;
+		return (-1);
 
-    	length = read(fd, buffer, longueur);
-
+	length = read(fd, buffer, longueur);
+	if (longueur > length)
+	{
+		buffer[length] = '\0';
+	}
 	if ((int)length == -1)
-		return 0;
-	printf("%s",buffer);		
+		return (0);
+	printf("%s", buffer);
 	free(buffer);
 	close(fd);
-	return length;
+	return (length);
 }
